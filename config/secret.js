ab355153017ccacddefd5b0d1172fb12a7f04176
jihaddeveloper <jihad.developer@gmail.com>
2019-02-27 11:05:40 +0600

    Getting ready for deployment.

--- config/secret.js
+++ config/secret.js
@@ -1,6 +1,6 @@
 module.exports = {
     database: 'mongodb://jihad:jihad1234@ds115353.mlab.com:15353/e-commerce_db',
-    port: 3000 || process.env.port,
+    port: process.env.PORT || 3000,
     secretKey: 'Jihad@Dev!', 
 
     facebook: {
